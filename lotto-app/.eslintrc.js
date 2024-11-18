5dfd01c63581ec30215773bc052ed05082bfa5c8
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-18 16:34:18 +0900

    Chore | 패키지 설정

---
+++ lotto-app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'no-undefined': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+} 
