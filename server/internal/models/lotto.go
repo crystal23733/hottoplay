73f53deb62bd4c711815449106cd8b3239847380
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-19 14:30:28 +0900

    Feat | Lotto데이터 구조체 추가

---
+++ server/internal/models/lotto.go
@@ -0,0 +1,19 @@
+package models
+
+// LottoData는 로또 데이터 구조를 정의합니다.
+type LottoData struct {
+	TotSellamnt    int    `json:"totSellamnt"`    // 총 판매 금액
+	FirstWinamnt   int    `json:"firstWinamnt"`   // 1등 당첨금
+	DrwNo          int    `json:"drwNo"`          // 회차 번호
+	DrwtNo1        int    `json:"drwtNo1"`        // 당첨 번호 1
+	DrwtNo2        int    `json:"drwtNo2"`        // 당첨 번호 2
+	DrwtNo3        int    `json:"drwtNo3"`        // 당첨 번호 3
+	DrwtNo4        int    `json:"drwtNo4"`        // 당첨 번호 4
+	DrwtNo5        int    `json:"drwtNo5"`        // 당첨 번호 5
+	DrwtNo6        int    `json:"drwtNo6"`        // 당첨 번호 6
+	BnusNo         int    `json:"bnusNo"`         // 보너스 번호
+	FirstPrzwnerCo int    `json:"firstPrzwnerCo"` // 1등 당첨자 수
+	FirstAccumamnt int    `json:"firstAccumamnt"` // 1등 누적 당첨금
+	ReturnValue    string `json:"returnValue"`    // 응답 상태 (예: success)
+	DrwNoDate      string `json:"drwNoDate"`      // 추첨 날짜
+}
