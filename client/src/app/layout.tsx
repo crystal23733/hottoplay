00962b1024d71b183caa7fbc2fdfea41b9f50a48
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-21 12:43:13 +0900

    Chore | 테일윈드 및 shadcn/ui 설정

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import '@/style/globals.css';
+import '../../style/globals.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
