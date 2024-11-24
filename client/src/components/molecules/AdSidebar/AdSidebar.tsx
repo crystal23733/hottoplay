e9d95e133e3ea43ddd49c6cbeeb5657844478650
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-24 17:36:28 +0900

    Refactor | 사이드바 광고 영역을 표시하는 컴포넌트

---
+++ client/src/components/molecules/AdSidebar/AdSidebar.tsx
@@ -0,0 +1,16 @@
+import { AdSpace } from '@/components/atoms/AdSpace/AdSpace';
+import AdSidebarProps from './AdSidebar.types';
+
+/**
+ * 사이드바 광고 영역을 표시하는 컴포넌트
+ */
+export const AdSidebar = ({ position, startAdNumber }: AdSidebarProps) => {
+  return (
+    <aside className="hidden md:block md:col-span-3">
+      <div className="space-y-6">
+        <AdSpace label={`광고 영역 ${startAdNumber}`} height="lg" />
+        <AdSpace label={`광고 영역 ${startAdNumber + 1}`} height="lg" />
+      </div>
+    </aside>
+  );
+};
