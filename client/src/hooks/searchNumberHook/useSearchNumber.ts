efa208dfce58422b78a40d7f8c79c35b7539344c
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-26 15:18:19 +0900

    Feat | 회차가 빈 문자열일 경우 API호출 X

--- client/src/hooks/searchNumberHook/useSearchNumber.ts
+++ client/src/hooks/searchNumberHook/useSearchNumber.ts
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 export default (round: string) => {
   const [data, setData] = useState<LottoRoundDetail>();
   useEffect(() => {
+    // round가 빈 문자열이면 API 요청을 보내지 않음
+    if (round === '') return;
     const fetchData = async () => {
       const response = await fetchRoundNumber(round);
       try {
