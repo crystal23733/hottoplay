d2c1f36a02e41dd7ff69a28596ce6cc7bd7b01e4
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-26 17:42:26 +0900

    Style | Loading컴포넌트 사이즈 스타일 추가

---
+++ client/src/components/atoms/Loading/LoadingSpinner.styles.ts
@@ -0,0 +1,9 @@
+import { SpinnerSize } from './LodingSpinner.types';
+
+const sizeClasses: Record<SpinnerSize, string> = {
+  sm: 'w-4 h-4',
+  md: 'w-8 h-8',
+  lg: 'w-12 h-12',
+};
+
+export default sizeClasses;
