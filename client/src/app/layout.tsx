48a5a15dc50004bf7f0dd5a069e85b9b10a3eace
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-29 00:55:49 +0900

    Feat | meta SEO 메타데이터 및 폰트 설정 업데이트
    - 페이지 설명 및 키워드 추가
    - Geist 폰트 패밀리 적용

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: 'hottoplay',
   description:
     '역대 로또 당첨 번호를 분석하여 새로운 번호 조합을 생성합니다. 유니크한 번호 조합, 가장 많이 당첨된 번호 기반 조합, 사용자가 제시하는 번호 조합 등 다양한 방식으로 제작자와 함께 번호를 생성해보세요.',
-  keywords: "로또, 로또번호생성, 로또분석, 당첨번호, 번호조합, 로또예측, 무료",
+  keywords: '로또, 로또번호생성, 로또분석, 당첨번호, 번호조합, 로또예측, 무료',
 };
 
 export default function RootLayout({
