ec0c479c96205bf1c7cf2348558fa97d9a2ab072
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-01 15:24:05 +0900

    Fix | PR 타입 검증 로직 수정
    - RELEASE_TO_MAIN을 RELEASE_TO_TEST_MAIN으로 수정
    - release/ 브랜치에서 test-main으로의 PR 인식 문제 해결
    - 워크플로우 트리거 조건 수정- RELEASE_TO_MAIN을 RELEASE_TO_TEST_MAIN으로 수정

--- .github/actions/services/PullRequestService.js
+++ .github/actions/services/PullRequestService.js
@@ -46,9 +46,9 @@ class PullRequestService {
       return 'RELEASE_TO_DEVELOP';
     }
 
-    if (ActionUtils.isValidBranch(base.ref, BRANCH_RULES.RELEASE_TO_MAIN.base) && 
-        ActionUtils.hasValidPrefix(head.ref, BRANCH_RULES.RELEASE_TO_MAIN.head)) {
-      return 'RELEASE_TO_MAIN';
+    if (ActionUtils.isValidBranch(base.ref, BRANCH_RULES.RELEASE_TO_TEST_MAIN.base) && 
+        ActionUtils.hasValidPrefix(head.ref, BRANCH_RULES.RELEASE_TO_TEST_MAIN.head)) {
+      return 'RELEASE_TO_TEST_MAIN';
     }
 
     return null;
