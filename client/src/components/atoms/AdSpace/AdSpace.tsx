59a4d4218cd08b7886e1f211e10ea7782b813606
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-04 11:23:37 +0900

    Refactor | 광고 컴포넌트 크기 설정 방식 개선
    내용:
    - height prop을 size로 변경
    - 기본 크기값을 medium-rectangle로 설정
    - 새로운 크기 설정 방식 적용
    - JSDoc 업데이트

--- client/src/components/atoms/AdSpace/AdSpace.tsx
+++ client/src/components/atoms/AdSpace/AdSpace.tsx
@@ -1,14 +1,14 @@
-import { AdSpaceProps, AD_SPACE_HEIGHTS } from './AdSpace.types';
+import { AdSpaceProps, AD_SPACE_SIZES } from './AdSpace.types';
 
 /**
- * 기본 광고 공간을 표시하는 컴포넌트
+ * 구글 애드센스 권장 크기에 맞춘 광고 공간을 표시하는 컴포넌트
  */
-export const AdSpace = ({ label, height = 'md', className = '' }: AdSpaceProps) => {
+export const AdSpace = ({ label, size = 'medium-rectangle', className = '' }: AdSpaceProps) => {
   return (
     <div
       className={`
       bg-muted rounded-lg
-      ${AD_SPACE_HEIGHTS[height]}
+      ${AD_SPACE_SIZES[size]}
       ${className}
     `}
     >
