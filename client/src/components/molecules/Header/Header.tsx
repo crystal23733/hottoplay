8cf3aac02d5f28a8db27fb97fa0ad0178453cb0a
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-19 17:24:56 +0900

    Feat | 공지사항 이동 버튼
    파일:
    - src/components/molecules/Header/Header.tsx
    - src/components/atoms/NoticeButton/NoticeButton.tsx

--- client/src/components/molecules/Header/Header.tsx
+++ client/src/components/molecules/Header/Header.tsx
@@ -1,10 +1,14 @@
 import Logo from '@/components/atoms/Logo/Logo';
+import NoticeLink from '../Notice/NoticeLink/NoticeLink';
 
 const Header: React.FC = () => (
   <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
     <nav className="container mx-auto px-4 h-16">
       <div className="flex h-full items-center justify-between">
         <Logo />
+        <div className="flex items-center gap-2">
+          <NoticeLink />
+        </div>
       </div>
     </nav>
   </header>
