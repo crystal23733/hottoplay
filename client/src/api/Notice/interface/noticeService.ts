8695835bc814196822026914d746e149be66b9b4
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-19 17:26:27 +0900

    Feat | API요청 함수 제작
    - 응답 인터페이스 제작
    - 함수 추상화 제작
    - 함수 구현체 제작
    
    파일:
    - src/api/Notice/notice.types.ts
    - src/api/Notice/interface/noticeService.ts
    - src/api/Notice/NoticeService.ts

---
+++ client/src/api/Notice/interface/noticeService.ts
@@ -0,0 +1,10 @@
+import { NoticeListResponse } from '../notice.types';
+
+/**
+ * 공지사항 서비스 인터페이스
+ * @interface
+ * @property {Function} getList - 공지사항 목록 조회
+ */
+export default interface INoticeService {
+  getList: (page?: number, limit?: number) => Promise<NoticeListResponse>;
+}
