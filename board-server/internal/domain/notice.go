9a4acdc81ed74648e4712f21dc4cd8f8554e3df9
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-21 12:28:56 +0900

    Refactor | domain 구조체 메모리 패딩
    - 구조체의 필드 순서를 바꿔 메모리 패딩 최적화
    
    파일:
    - ../board-server/internal/domain/notice.go

--- board-server/internal/domain/notice.go
+++ board-server/internal/domain/notice.go
@@ -8,16 +8,16 @@ import (
 
 type Notice struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
-	Title     string             `json:"title" bson:"title"`
-	Content   string             `json:"content" bson:"content"`
 	Author    primitive.ObjectID `json:"author_id" bson:"author_id"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	Title     string             `json:"title" bson:"title"`
+	Content   string             `json:"content" bson:"content"`
 	Timestamp string             `json:"timestamp" bson:"timestamp"`
 }
 
 // User는 사용자의 도메인 모델을 정의한다.
 type User struct {
-	Name   string               `json:"name" bson:"name"`
 	Notice []primitive.ObjectID `json:"notices" bson:"notices"`
+	Name   string               `json:"name" bson:"name"`
 }
