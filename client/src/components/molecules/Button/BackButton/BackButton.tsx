a0638abd657bdf6ea7cb694a36a07e24b35418b0
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-23 11:49:23 +0900

    Feat | 뒤로가기 버튼 제작
    - 자주 사용될 뒤로가기 버튼 원자 단위 제작
    
    파일:
    - src/components/molecules/Button/BackButton/BackButton.tsx

---
+++ client/src/components/molecules/Button/BackButton/BackButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/ui/Button';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+const BackButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button variant="ghost" className="mb-6" onClick={() => router.back()}>
+      <ArrowLeft className="mr-2 h-4 w-4" />
+      목록으로
+    </Button>
+  );
+};
+
+export default BackButton;
