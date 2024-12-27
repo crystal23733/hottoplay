251f9ccc413048275b818efdaaac75449f70cae3
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-27 17:58:35 +0900

    Fix | 서버 별 헬스 라우터 생성

--- server/internal/api/router/router.go
+++ server/internal/api/router/router.go
@@ -12,22 +12,24 @@ import (
 func NewRouter(lottoHandler *handler.LottoHandler, apiKey string) *mux.Router {
 	r := mux.NewRouter()
 
+	// API 서브라우터 설정
+	api := r.PathPrefix("/api/v1").Subrouter()
+
 	// 기본 라우트 추가 (헬스 체크용)
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	api.HandleFunc("/lotto", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"message": "Hello, World!"}`))
 	}).Methods(http.MethodGet)
 
-	// API 서브라우터 설정
-	api := r.PathPrefix("/api/v1").Subrouter()
-
 	// 미들웨어 적용
-	api.Use(middleware.APIKeyMiddleware(apiKey))
+	// 나머지 경로에 대해 미들웨어 적용
+	protectedAPI := api.PathPrefix("").Subrouter()
+	protectedAPI.Use(middleware.APIKeyMiddleware(apiKey))
 
 	// 엔드포인트 설정
-	api.HandleFunc("/lotto/numbers", lottoHandler.GenerateNumbers).Methods(http.MethodPost)
-	api.HandleFunc("/lotto/rounds", lottoHandler.GetRoundNumbers).Methods(http.MethodGet)
-	api.HandleFunc("/lotto/populars", lottoHandler.GetPopularWatch).Methods(http.MethodGet)
+	protectedAPI.HandleFunc("/lotto/numbers", lottoHandler.GenerateNumbers).Methods(http.MethodPost)
+	protectedAPI.HandleFunc("/lotto/rounds", lottoHandler.GetRoundNumbers).Methods(http.MethodGet)
+	protectedAPI.HandleFunc("/lotto/populars", lottoHandler.GetPopularWatch).Methods(http.MethodGet)
 
 	return r
 }
