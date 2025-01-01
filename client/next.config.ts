324876fbbeeb93fe04f5c610e04d74dc2d81c4b9
Hoyeong Yoo <crystal23733@gmail.com>
2025-01-01 18:10:38 +0900

    Fix | 배포 설정 변
    - 앰플리파이 설정 변경
    - nextJS
      - URL 끝 슬래시 제거
      - // Amplify에서 이미지 최적화 비활성화
      - URL 재작성 규칙
        - notices/ 이후의 모든 경로
        - 동일한 경로로 내부 라우팅

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  trailingSlash: false,
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/notices/:path*',
+        destination: '/notices/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
