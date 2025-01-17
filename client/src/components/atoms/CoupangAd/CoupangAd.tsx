a9a33f2b882d6f3407ebd78bae59f2fc300ab98f
Hoyeong Yoo <crystal23733@gmail.com>
2025-01-17 11:22:02 +0900

    Docs | CoupangAd JSDoc 추가

--- client/src/components/atoms/CoupangAd/CoupangAd.tsx
+++ client/src/components/atoms/CoupangAd/CoupangAd.tsx
@@ -1,5 +1,10 @@
 import { COUPANG_AD_SIZES, CoupangAdProps } from './CoupangAd.types';
 
+/**
+ * 쿠팡 파트너스 광고 컴포넌트
+ * @param props 쿠팡 파트너스 광고 Props
+ * @returns 쿠팡 파트너스 광고 컴포넌트
+ */
 export const CoupangAd: React.FC<CoupangAdProps> = ({ size = 'rectangle', className }) => {
   return (
     <div className={`${COUPANG_AD_SIZES[size]} ${className}`}>
