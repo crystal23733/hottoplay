8b1863e530793778b3f3c06fb9e428103f84bae8
Hoyeong Yoo <crystal23733@gmail.com>
2025-01-17 16:37:23 +0900

    Refactor | 꿈 페이지 사이드바 광고 할당

--- client/src/components/features/Dream/DreamTemplate.tsx
+++ client/src/components/features/Dream/DreamTemplate.tsx
@@ -9,7 +9,7 @@ const DreamTemplate = () => {
   return (
     <MainLayout showAd={false}>
       <div className="bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 dark:from-blue-950/30 dark:via-indigo-950/30 dark:to-purple-950/30 min-h-screen pt-8 pb-12">
-        <CoupangLayout>
+        <CoupangLayout showSidebar={true}>
           <div className="container mx-auto px-4">
             <div className="max-w-4xl mx-auto">
               <BackButton>홈으로</BackButton>
