d3995b7f1267d8fb40db81d80c58e267c4992db1
Hoyeong Yoo <crystal23733@gmail.com>
2025-01-22 12:04:02 +0900

    Test | 컴포넌트 수정에 따른 단위테스트 수정

--- client/src/components/molecules/PatternOptions/__test__/PatternOptions.test.tsx
+++ client/src/components/molecules/PatternOptions/__test__/PatternOptions.test.tsx
@@ -11,23 +11,23 @@ describe('PatternOptions', () => {
   it('renders all pattern options', () => {
     render(<PatternOptions value="sequential" onChange={mockOnChange} />);
 
-    expect(screen.getByLabelText('연속된 숫자 포함')).toBeInTheDocument();
-    expect(screen.getByLabelText('홀짝 비율 조정')).toBeInTheDocument();
-    expect(screen.getByLabelText('구간별 분배')).toBeInTheDocument();
+    expect(screen.getByLabelText('연속 번호 포함')).toBeInTheDocument();
+    expect(screen.getByLabelText('홀짝 균형 조합')).toBeInTheDocument();
+    expect(screen.getByLabelText('구간별 안배 조합')).toBeInTheDocument();
   });
 
   it('shows correct selected value', () => {
     render(<PatternOptions value="sequential" onChange={mockOnChange} />);
 
-    expect(screen.getByLabelText('연속된 숫자 포함')).toBeChecked();
-    expect(screen.getByLabelText('홀짝 비율 조정')).not.toBeChecked();
-    expect(screen.getByLabelText('구간별 분배')).not.toBeChecked();
+    expect(screen.getByLabelText('연속 번호 포함')).toBeChecked();
+    expect(screen.getByLabelText('홀짝 균형 조합')).not.toBeChecked();
+    expect(screen.getByLabelText('구간별 안배 조합')).not.toBeChecked();
   });
 
   it('calls onChange when selection changes', () => {
     render(<PatternOptions value="sequential" onChange={mockOnChange} />);
 
-    fireEvent.click(screen.getByLabelText('홀짝 비율 조정'));
+    fireEvent.click(screen.getByLabelText('홀짝 균형 조합'));
     expect(mockOnChange).toHaveBeenCalledWith('oddEven');
   });
 });
--- client/src/components/molecules/StatisticsOptions/__test__/StatisticsOptions.test.tsx
+++ client/src/components/molecules/StatisticsOptions/__test__/StatisticsOptions.test.tsx
@@ -11,23 +11,23 @@ describe('StatisticsOptions', () => {
   it('renders all statistics options', () => {
     render(<StatisticsOptions value="hot" onChange={mockOnChange} />);
 
-    expect(screen.getByLabelText(/핫 넘버/)).toBeInTheDocument();
-    expect(screen.getByLabelText(/콜드 넘버/)).toBeInTheDocument();
-    expect(screen.getByLabelText(/밸런스/)).toBeInTheDocument();
-    expect(screen.getByLabelText(/가중치 기반/)).toBeInTheDocument();
+    expect(screen.getByLabelText('상위 빈도 조합')).toBeInTheDocument();
+    expect(screen.getByLabelText('최근 미출현 번호')).toBeInTheDocument();
+    expect(screen.getByLabelText('구간별 빈도 조합')).toBeInTheDocument();
+    expect(screen.getByLabelText('확률 가중치 기반')).toBeInTheDocument();
   });
 
   it('shows correct selected value', () => {
     render(<StatisticsOptions value="hot" onChange={mockOnChange} />);
 
-    expect(screen.getByLabelText(/핫 넘버/)).toBeChecked();
-    expect(screen.getByLabelText(/콜드 넘버/)).not.toBeChecked();
+    expect(screen.getByLabelText('상위 빈도 조합')).toBeChecked();
+    expect(screen.getByLabelText('최근 미출현 번호')).not.toBeChecked();
   });
 
   it('calls onChange when selection changes', () => {
     render(<StatisticsOptions value="hot" onChange={mockOnChange} />);
 
-    fireEvent.click(screen.getByLabelText(/콜드 넘버/));
+    fireEvent.click(screen.getByLabelText('최근 미출현 번호'));
     expect(mockOnChange).toHaveBeenCalledWith('cold');
   });
 });
