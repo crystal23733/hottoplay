6aa823dfe6834d459f5a8e8f5d7d0825475a9208
Hoyeong Yoo <crystal23733@gmail.com>
2025-02-08 15:03:35 +0900

    Feat | 라우터 변경
    - 기존 페이지 /lotto변경

--- client/src/app/lotto/layout.tsx
+++ client/src/app/lotto/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import '../../style/globals.css';
+import '../../../style/globals.css';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import { Toaster } from '@/ui/toaster';
 import env from '@/config/meta/env';
 import hottoplay from '@/lotto/constants/hottoplay';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
