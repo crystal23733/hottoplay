ec6b357ff8658e8276c4ab080642f1eb9fef2824
Hoyeong Yoo <crystal23733@gmail.com>
2025-02-10 20:25:39 +0900

    Feat | 기능 그리드 컴포넌트 제작
    파일:
    - src/components/organisms/FeatureGrid/FeatureGrid.tsx
    - src/components/organisms/FeatureGrid/FeatureGrid.types.ts

---
+++ client/src/components/organisms/FeatureGrid/FeatureGrid.types.ts
@@ -0,0 +1,10 @@
+import FeatureCardProps from '@/components/molecules/FeatureCard/FeatureCard.types';
+
+/**
+ * 기능 그리드 인터페이스
+ * @property {FeatureCardProps[]} features - 기능 목록
+ */
+export default interface FeatureGridProps {
+  features: FeatureCardProps[];
+}
+
