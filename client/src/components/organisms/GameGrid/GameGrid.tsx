73b47ab1d6ce1ac494f25893093995d902a80e10
Hoyeong Yoo <crystal23733@gmail.com>
2025-02-10 20:24:39 +0900

    Feat | 게임 목록 컴포넌트 제작
    파일:
    - src/components/organisms/GameGrid/GameGrid.types.ts
    - src/components/organisms/GameGrid/GameGrid.tsx

---
+++ client/src/components/organisms/GameGrid/GameGrid.tsx
@@ -0,0 +1,18 @@
+import GameCard from '@/components/molecules/GameCard/GameCard';
+import GameGridProps from './GameGrid.types';
+
+/**
+ * 게임 그리드
+ * @param {GameCardProps[]} games - 게임 목록
+ */
+const GameGrid: React.FC<GameGridProps> = ({ games }) => {
+  return (
+    <div className="grid md:grid-cols-2 gap-8">
+      {games.map(game => (
+        <GameCard key={game.href} {...game} />
+      ))}
+    </div>
+  );
+};
+
+export default GameGrid;
