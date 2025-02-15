3d38cb8ff1d95050fd5ee41565a2a218ffd00960
Hoyeong Yoo <crystal23733@gmail.com>
2025-02-15 22:20:08 +0900

    1159회차 업데이트

--- client/src/components/features/SearchNumbers.tsx
+++ client/src/components/features/SearchNumbers.tsx
@@ -45,9 +45,9 @@ export default function SearchNumbers() {
 
   const roundOptions = useMemo(
     () =>
-      Array.from({ length: 1158 }, (_, i) => ({
-        value: String(1158 - i),
-        label: `${1158 - i}회차`,
+      Array.from({ length: 1159 }, (_, i) => ({
+        value: String(1159 - i),
+        label: `${1159 - i}회차`,
       })),
     []
   );
