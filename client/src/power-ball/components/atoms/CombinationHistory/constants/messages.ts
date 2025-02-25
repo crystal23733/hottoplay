631a2660449d1579b6ef057c2b17d846b493995c
Hoyeong Yoo <crystal23733@gmail.com>
2025-02-25 19:42:11 +0900

    Feat | 번호 조합 이력 컴포넌트 리터럴값
    - 리터럴값 분리
    
    파일:
    - src/power-ball/components/atoms/CombinationHistory/constants/messages.ts

---
+++ client/src/power-ball/components/atoms/CombinationHistory/constants/messages.ts
@@ -0,0 +1,11 @@
+const MESSAGES = {
+  APPEARANCES: {
+    NEVER: 'This combination has never appeared',
+    ONCE: 'This combination has appeared once',
+    MULTIPLE: 'This combination has appeared',
+    TIMES: 'times',
+  },
+  LAST_SEEN: 'Last seen',
+};
+
+export default MESSAGES;
