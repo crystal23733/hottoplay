58460ce7f25620e220e03ffba9a3091dd88466fb
Hoyeong Yoo <crystal23733@gmail.com>
2025-04-07 11:41:38 +0900

    Feat|페이지 제작
    파일:
    - src/app/power-ball/frequency/page.tsx
    - src/app/power-ball/frequency/layout.tsx

---
+++ client/src/app/power-ball/frequency/layout.tsx
@@ -0,0 +1,7 @@
+export default function PowerBallFrequencyLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
---
+++ client/src/app/power-ball/frequency/page.tsx
@@ -0,0 +1,5 @@
+import PowerBallFrequency from '@/power-ball/components/features/PowerBallFrequency/PowerBallFrequency';
+
+export default function PowerBallFrequencyPage() {
+  return <PowerBallFrequency />;
+}
