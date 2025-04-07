f6663518f56f6936531e48f8c507a305635d3cd6
Hoyeong Yoo <crystal23733@gmail.com>
2025-04-07 23:20:10 +0900

    i

---
+++ client/src/app/power-ball/draws/draw-detail/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Loading from '@/lotto/components/atoms/Loading/Loading';
+import PowerBallDrawDetail from '@/power-ball/components/features/PowerBallDrawDetail/PowerBallDrawDetail';
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+
+function DrawDetailContent() {
+  const searchParams = useSearchParams();
+  const date = searchParams.get('date');
+
+  if (!date) {
+    return <div>날짜 정보가 없습니다.</div>;
+  }
+
+  return <PowerBallDrawDetail date={date} />;
+}
+
+export default function DrawDetailPage() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <DrawDetailContent />
+    </Suspense>
+  );
+}
