4e6c8a6a38dfb0193cae5f7b2ee35b409ea49041
Hoyeong Yoo <crystal23733@gmail.com>
2025-04-07 23:02:10 +0900

    i

--- client/src/app/power-ball/draws/[date]/page.tsx
+++ client/src/app/power-ball/draws/[date]/page.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-import Loading from '@/lotto/components/atoms/Loading/Loading';
 import PowerBallDrawDetail from '@/power-ball/components/features/PowerBallDrawDetail/PowerBallDrawDetail';
 import { useParams } from 'next/navigation';
-import { Suspense } from 'react';
 
-function DrawDetailContent() {
+export default function PowerBallDrawDetailPage() {
   const params = useParams();
   const date = params.date as string;
   const decodedDate = decodeURIComponent(date);
 
   return <PowerBallDrawDetail date={decodedDate} />;
-}
-
-export default function PowerBallDrawDetailPage() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <DrawDetailContent />
-    </Suspense>
-  );
 }
