9069f333e9cfd4d990cedc892e1de155160e7885
Hoyeong Yoo <crystal23733@gmail.com>
2025-04-14 16:20:57 +0900

    Feat|Hot 번호 기반 복권 번호 생성기 구현
    - 과거 추첨 데이터를 기반으로 번호 출현 빈도를 분석하는 calculateFrequency 함수 추가
    - 번호별 출현 횟수를 정렬하여 자주 나온 순서대로 가져오는 getFrequencySortedNumbers 함수 구현
    - 자주 등장한 번호들 중 무작위로 번호를 골라 조합을 생성하는 GenerateHotNumbers 함수 구현
      - 흰 공은 상위 15개 중 5개 랜덤 선택
      - 메가볼은 상위 5개 중 1개 랜덤 선택
    - 생성된 번호는 정렬되어 반환되며, models.GeneratedNumbers 형태로 출력됨
    - 향후 추천 기능이나 번호 예측 모델에 활용 가능
    
    파일:
    - internal/generator/generator.go

--- megamillions-lambda/internal/generator/generator.go
+++ megamillions-lambda/internal/generator/generator.go
@@ -17,6 +17,12 @@ type Generator struct {
 	rand                 *rand.Rand
 }
 
+// NumberFreq는 번호와 빈도를 나타내는 구조체입니다.
+type NumberFreq struct {
+	Number int
+	Freq   int
+}
+
 // calculateFrequency는 번호별 출현 빈도를 계산합니다.
 func (g *Generator) calculateFrequency() {
 	for _, draw := range g.draws {
@@ -25,6 +31,24 @@ func (g *Generator) calculateFrequency() {
 		}
 		g.megaBallFrequency[draw.MegaBall]++
 	}
+}
+
+// getFrequencySortedNumbers는 주어진 빈도 맵에서 정렬된 NumberFreq 슬라이스를 반환합니다.
+func getFrequencySortedNumbers(freqMap map[int]int, ascending bool) []NumberFreq {
+	freqs := make([]NumberFreq, 0, len(freqMap))
+	for num, freq := range freqMap {
+		freqs = append(freqs, NumberFreq{Number: num, Freq: freq})
+	}
+
+	// 빈도 정렬 (ascending이 true면 오름차순, false면 내림차순)
+	sort.Slice(freqs, func(i, j int) bool {
+		if ascending {
+			return freqs[i].Freq < freqs[j].Freq
+		}
+		return freqs[i].Freq > freqs[j].Freq
+	})
+
+	return freqs
 }
 
 // NewGenerator는 새로운 Generator 인스턴스를 생성합니다.
@@ -68,6 +92,49 @@ func (g *Generator) GenerateRandom() models.GeneratedNumbers {
 
 	// 메가볼 번호 생성
 	megaBall := g.rand.Intn(g.megaBallRange[1]-g.megaBallRange[0]+1) + g.megaBallRange[0]
+
+	return models.GeneratedNumbers{
+		WhiteNumbers: whiteNumbers,
+		MegaBall:     megaBall,
+	}
+}
+
+// GenerateHotNumbers는 자주 나오는 번호를 기반으로 번호 조합을 생성합니다.
+func (g *Generator) GenerateHotNumbers() models.GeneratedNumbers {
+	// 흰 공 빈도 정렬
+	whiteFreqs := getFrequencySortedNumbers(g.whiteNumberFrequency, false)
+
+	// 상위 빈도 번호에서 랜덤 선택 (상위 15개 중 5개)
+	numHotWhiteNumbers := len(whiteFreqs)
+	if numHotWhiteNumbers > 15 {
+		numHotWhiteNumbers = 15
+	}
+
+	whiteNumbers := make([]int, 0, 5)
+	whiteNumbersMap := make(map[int]bool)
+
+	for len(whiteNumbers) < 5 {
+		idx := g.rand.Intn(numHotWhiteNumbers)
+		num := whiteFreqs[idx].Number
+		if !whiteNumbersMap[num] {
+			whiteNumbers = append(whiteNumbers, num)
+			whiteNumbersMap[num] = true
+		}
+	}
+
+	// 오름차순 정렬
+	sort.Ints(whiteNumbers)
+
+	// 메가볼 빈도 정렬
+	megaFreqs := getFrequencySortedNumbers(g.megaBallFrequency, false)
+
+	// 상위 빈도 메가볼에서 선택 (상위 5개 중 1개)
+	numHotMegaBalls := len(megaFreqs)
+	if numHotMegaBalls > 5 {
+		numHotMegaBalls = 5
+	}
+
+	megaBall := megaFreqs[g.rand.Intn(numHotMegaBalls)].Number
 
 	return models.GeneratedNumbers{
 		WhiteNumbers: whiteNumbers,
