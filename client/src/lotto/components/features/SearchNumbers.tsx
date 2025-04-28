1e1050d88d134754391c70831774965c272ff5b6
Hoyeong Yoo <crystal23733@gmail.com>
2025-04-28 10:14:25 +0900

    1168회차 업데이트

--- client/src/lotto/components/features/SearchNumbers.tsx
+++ client/src/lotto/components/features/SearchNumbers.tsx
@@ -45,9 +45,9 @@ export default function SearchNumbers() {
 
   const roundOptions = useMemo(
     () =>
-      Array.from({ length: 1168 }, (_, i) => ({
-        value: String(1168 - i),
-        label: `${1168 - i}회차`,
+      Array.from({ length: 1169 }, (_, i) => ({
+        value: String(1169 - i),
+        label: `${1169 - i}회차`,
       })),
     []
   );
