1062a2e2a7ff603a5f92fc57a9dc7076a4c149d9
Hoyeong Yoo <crystal23733@gmail.com>
2025-05-01 11:45:37 +0900

    Feat|NumberStatCard 복사 및 수정
    파일:
    - src/megamillions/components/atoms/NumberStatCard/constants/styles.ts
    - src/megamillions/components/atoms/NumberStatCard/constants/messages.ts
    - src/megamillions/components/atoms/NumberStatCard/NumberStatCard.types.ts
    - src/megamillions/components/atoms/NumberStatCard/NumberStatCard.tsx

---
+++ client/src/megamillions/components/atoms/NumberStatCard/constants/messages.ts
@@ -0,0 +1,17 @@
+const MESSAGES = {
+  TOTAL_APPEARANCES: 'Total Appearances',
+  WHITE_BALL: {
+    TITLE: 'White Ball',
+    APPEARED: 'Appeared',
+    TIMES: 'times',
+    LAST_DRAWN: 'Last drawn',
+  },
+  MEGA_BALL: {
+    TITLE: 'Mega Ball',
+    APPEARED: 'Appeared',
+    TIMES: 'times',
+    LAST_DRAWN: 'Last drawn',
+  },
+};
+
+export default MESSAGES;
