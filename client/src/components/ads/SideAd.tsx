d70ed68eb2eb564370bfd94568b0551e2216b888
Hoyeong Yoo <crystal23733@gmail.com>
2025-09-12 10:42:11 +0900

    Squashed commit of the following:
    commit a48c62a2587761d8f2ab066136dbdd9fe10e7a58
    Author: Hoyeong Yoo <crystal23733@gmail.com>
    Date:   Fri Sep 12 10:40:17 2025 +0900
    
        refactor|광고 컴포넌트 새로고침
    
    commit 292cb092ab9f465c41bda8701464296d74852c27
    Author: Hoyeong Yoo <crystal23733@gmail.com>
    Date:   Wed Sep 10 16:52:59 2025 +0900
    
        refactor(comp)|오른쪽 광고 컴포넌트 변경

--- client/src/components/ads/SideAd.tsx
+++ client/src/components/ads/SideAd.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 
 declare global {
   interface Window {
@@ -13,9 +14,15 @@ declare global {
  * 사이드 광고 컴포넌트
  */
 export default function SideAd() {
+  const pathname = usePathname();
+
   useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, []);
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.error('SideAd 로드 실패:', error);
+    }
+  }, [pathname]); // pathname 변경 시마다 새로운 광고 로드
 
   return (
     <div className="w-full flex justify-center">
